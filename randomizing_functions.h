3fe88ae98ac1a6eea45440f8add473763d313294
Erik Johnson <joh06937@gmail.com>
2021-11-05 18:27:22 -0500

    Randomizing Functions: Cleaned up whitespace and formatting

--- randomizing_functions.h
+++ randomizing_functions.h
@@ -1,10 +1,11 @@
 #pragma once
 
 // Initialize a structure that will hold the output of randomizeTime
-struct time_outputs {
-  uint32_t cumulativeTime;
-  int count;
+struct time_outputs
+{
+    uint32_t cumulativeTime;
+    int count;
 };
 
 struct time_outputs randomizeTime(void);
-static void randomizeSpeed(time_outputs randomTime, uint32_t TotalTime, uint32_t StartRestTime); 
+void randomizeSpeed(time_outputs randomTime, uint32_t TotalTime, uint32_t StartRestTime);
--- randomizing_functions.ino
+++ randomizing_functions.ino
@@ -1,86 +1,93 @@
 #include "randomizing_functions.h"
 
-// Helper functions run at setup. 
+// Helper functions run at setup.
 
-//Writes a function that randomizes the time spent in each stage. 
-struct time_outputs randomizeTime(void) 
-  {
-  // Create a random seed for random sequence generator.
-  randomSeed(analogRead(A5));
+//Writes a function that randomizes the time spent in each stage.
+struct time_outputs randomizeTime(void)
+{
+    // Create a random seed for random sequence generator.
+    randomSeed(analogRead(A5));
 
-  // Initialize the variable you'll use to keep track of how much of the working time has been used.
-  uint32_t cumulativeTime = 0;
+    // Initialize the variable you'll use to keep track of how much of the
+    // working time has been used.
+    uint32_t cumulativeTime = 0;
 
-  // Calculate the "working" time you can use for possible stages; Subtract the max stage time becuase the last stage added can go over the timeWorking limit. 
-  uint32_t timeWorking = TotalTime - StartRestTime - MinEndRestTime - MaxStageTime * 1000;
+    // Calculate the "working" time you can use for possible stages; Subtract
+    // the max stage time becuase the last stage added can go over the
+    // timeWorking limit.
+    uint32_t timeWorking = TotalTime - StartRestTime - MinEndRestTime - MaxStageTime * 1000;
 
-  //Initialize counter for putting into stageParameters; Start at 1 (instead of 0) because first position will be the initial rest
-  int count = 1;
+    // Initialize counter for putting into stageParameters; Start at 1 (instead
+    // of 0) because first position will be the initial rest
+    int count = 1;
 
-  while (cumulativeTime < timeWorking )
-  {
-    // Find a random time
-    uint32_t stagetime = random(MinStageTime, MaxStageTime) * (1000);
-
-    if (count >= ARRAY_SIZE(stageParameters))
+    while (cumulativeTime < timeWorking)
     {
-      // We usea pre-allocated static array of stage parameters to avoid dynamic
-      // memory allocation. If this is printed, then the list of generated random times
-      // was mostly short times, and there weren't enough places in the array
-      // to contain all of the stages. If you use short minimum stage times and need more stages,
-      // increase the number of elements in stageParameters above.
-      Serial.println("Overran pre-allocated size of possible motor stages, stopping early");
-      break;
+        // Find a random time
+        uint32_t stagetime = random(MinStageTime, MaxStageTime) * (1000);
+
+        if (count >= ARRAY_SIZE(stageParameters))
+        {
+            // We usea pre-allocated static array of stage parameters to avoid
+            // dynamic memory allocation. If this is printed, then the list of
+            // generated random times was mostly short times, and there weren't
+            // enough places in the array to contain all of the stages. If you
+            // use short minimum stage times and need more stages, increase the
+            // number of elements in stageParameters above.
+            Serial.println("Overran pre-allocated size of possible motor stages, stopping early");
+            break;
+        }
+
+        // Put the stagetime into the time part of the parameter list.
+        stageParameters[count].duration = stagetime;
+
+        // Calculate new cumulative time.
+        cumulativeTime = cumulativeTime + stagetime;
+
+        // Increase counter.
+        count = count + 1;
     }
 
-    // Put the stagetime into the time part of the parameter list.
-    stageParameters[count].duration = stagetime;
+    // Calculate the end resting time (will be the minimum rest time plus
+    // anything from the division of stages that was left over);
+    uint32_t EndRestTime = TotalTime - StartRestTime - cumulativeTime;
 
-    // Calculate new cumulative time.
-    cumulativeTime = cumulativeTime + stagetime;
+    // Set up end rest parameters
+    stageParameters[0].duration = StartRestTime;
 
-    // Increase counter.
-    count = count + 1;
-  }
+    // Set up end rest duration
+    stageParameters[count].duration = EndRestTime;
 
-   // Calculate the end resting time (will be the minimum rest time plus anything from the division of stages that was left over);
-  uint32_t EndRestTime = TotalTime - StartRestTime - cumulativeTime;
- 
-  // Set up end rest parameters
-  stageParameters[0].duration = StartRestTime;
+    struct time_outputs result = {
+        .cumulativeTime = cumulativeTime,
+        .count = count
+    };
 
-  // Set up end rest duration
-  stageParameters[count].duration = EndRestTime;
-  
-  struct time_outputs result = {
-    .cumulativeTime = cumulativeTime,
-    .count = count
-  };
-
-  return result;
+    return result;
 }
 
 // Creates a function that randomizes the speed order
-static void randomizeSpeed(time_outputs randomTime, uint32_t TotalTime, uint32_t StartRestTime)
+void randomizeSpeed(time_outputs randomTime, uint32_t TotalTime, uint32_t StartRestTime)
 {
-  // Creates a random seed for random sequence generator.
-  randomSeed(analogRead(A5));
+    // Creates a random seed for random sequence generator.
+    randomSeed(analogRead(A5));
 
-  // use the counter from above. Don't include "count", because that is the final rest period.  
-  // Don't include index "0" because that is the initial rest period. 
-  for (size_t i = 1; i < randomTime.count ; i++)
-  {
-    // Pick a random index within the speed array
-    size_t j = random(0, ARRAY_SIZE(allSpeeds));
+    // Use the counter from above. Don't include "count", because that is the
+    // final rest period.
+    //
+    // Don't include index "0" because that is the initial rest period.
+    for (size_t i = 1; i < randomTime.count ; i++)
+    {
+        // Pick a random index within the speed array
+        size_t j = random(0, ARRAY_SIZE(allSpeeds));
 
-    // Place in parameter array
-    stageParameters[i].speed = allSpeeds[j];
-  }
-  
-  // Set start rest speed.
-  stageParameters[0].speed = 0;
-  
-  // Set end rest speed.
-  stageParameters[randomTime.count].speed = 0;
+        // Place in parameter array
+        stageParameters[i].speed = allSpeeds[j];
+    }
 
+    // Set start rest speed.
+    stageParameters[0].speed = 0;
+
+    // Set end rest speed.
+    stageParameters[randomTime.count].speed = 0;
 }
