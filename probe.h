6aa430e832199551f8abdac3a0d951bb79126a9e
Erik Johnson <joh06937@gmail.com>
2021-11-05 18:28:29 -0500

    Probe: Cleaned up whitespace and formatting

--- probe.h
+++ probe.h
@@ -1,16 +1,17 @@
 #pragma once
 
-      
-enum class Probe 
+enum class Probe
 {
-  None,
-  NoWarning,
-  NoChange,
+    None,
+    NoWarning,
+    NoChange,
 };
 
-struct Probe_Messages {
-  int activity_tag;
-  String probe_string;
+struct Probe_Messages
+{
+    int activity_tag;
+    String probe_string;
 };
-void probeTrials(bool useProbeTrials, int count, double probability); 
+
+void probeTrials(bool useProbeTrials, int count, double probability);
 struct Probe_Messages getProbeMessages(enum Probe probeName);
--- probe.ino
+++ probe.ino
@@ -1,71 +1,74 @@
 
 #include "probe.h"
 
-// Assign probe trials. Input total number of trials and the probability of any kind of probe trial happening 
-void probeTrials(bool useProbeTrials, int count, double probability){
-  
-  // Don't use the 0 index or the last entry of stageParameters, because those will be the first and last rests.
-  for (int i = 1; i < count; i++){
+// Assign probe trials. Input total number of trials and the probability of any
+// kind of probe trial happening
+void probeTrials(bool useProbeTrials, int count, double probability)
+{
+    // Don't use the 0 index or the last entry of stageParameters, because those
+    // will be the first and last rests
+    for (int i = 1; i < count; i++)
+    {
+        // Default the probe in stageParameters to be "None"
+        stageParameters[i].probe = Probe::None;
 
-     // Default the probe in stageParameters to be "None" 
-     stageParameters[i].probe = Probe::None;
+        // If the user doesn't want to use probe trials, skip changing them to
+        // probe
+        if (!useProbeTrials)
+        {
+            continue;
+        }
 
-     // If the user wants to use probe trials, go about changing them to probe
-     if (useProbeTrials) {
-     
-     // Get a random number from 1 to 100
-     randomSeed(analogRead(A5));
-     double prob1 = random(1, 101)/100;
+        // Get a random number from 1 to 100
+        randomSeed(analogRead(A5));
 
-     // If that number is below the entered probability, this will be a probe trial. 
+        double prob1 = random(1, 101) / 100;
 
-     if (prob1 < probability){
+        // If that number is equal to or above the entered probability, this
+        // will not be a probe trial
+        if (prob1 >= probability)
+        {
+            continue;
+        }
 
-         // Now, get a random number either 0 or 1
-         randomSeed(analogRead(A5));
-         int prob2 = random(0, 2);
+        // Now, get a random number either 0 or 1
+        randomSeed(analogRead(A5));
+        int prob2 = random(0, 2);
 
-         switch (prob2){
-            
-            case 0: 
- 
-              stageParameters[i].probe = Probe::NoWarning;
+        switch (prob2)
+        {
+            case 0:
+                stageParameters[i].probe = Probe::NoWarning;
+                break;
 
-              break;
-            
             case 1:
-
-              stageParameters[i].probe = Probe::NoChange;
-              
-              break;
-           
-         }
-     } 
+                stageParameters[i].probe = Probe::NoChange;
+                break;
+        }
     }
-  }
 }
 
 // Both of these will be reported at the motor's time.
-struct Probe_Messages getProbeMessages(Probe probeName) {
+struct Probe_Messages getProbeMessages(Probe probeName)
+{
+    struct Probe_Messages probe_messages;
 
-  struct Probe_Messages probe_messages;
-  
-  switch (probeName){
-    
-    case Probe::NoWarning: 
+    switch (probeName)
     {
-        probe_messages.activity_tag = 13;
-        probe_messages.probe_string = "Probe: no warning tone."; 
-        return probe_messages; 
-      break; 
+        case Probe::NoWarning:
+        {
+            probe_messages.activity_tag = 13;
+            probe_messages.probe_string = "Probe: no warning tone.";
+            break;
+        }
+
+        case Probe::NoChange:
+        {
+            probe_messages.activity_tag = 14;
+            probe_messages.probe_string = "Probe: no change in motor.";
+            break;
+        }
     }
 
-    case Probe::NoChange: 
-    {
-        probe_messages.activity_tag = 14;
-        probe_messages.probe_string = "Probe: no change in motor."; 
-        return probe_messages; 
-      break; 
-    }
-    }
+    return probe_messages;
 }
